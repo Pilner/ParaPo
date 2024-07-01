ac2760ddc1fa5c96ffcae44ae4c106a7e19d0f48
Pilner <raileyvictuelles@gmail.com>
2024-07-01 10:43:05 +0800

    Frontend: Added Error Page - Added 404 Not Found error page to catch all unknown routes

---
+++ frontend/src/app/not-found.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import styles from "./notfound.module.css";
+import Navbar from "@/_components/semantics/Navbar";
+import Footer from "@/_components/semantics/Footer";
+import Button from "@/_components/Button";
+
+export default function NotFound() {
+  return (
+		<>
+			<Navbar />
+			<section id={styles.notFoundPage}>
+				<div className="container">
+					<div className={styles.errorPicture}>
+						<Image
+							src="/images/questionmark.png"
+							alt="Not Found Question Mark Picture"
+							width={0}
+							height={0}
+							style={{
+								width: "auto",
+								height: "100%",
+							}}
+							unoptimized={true}
+						/>
+					</div>
+					<h1 className="heroTitleFont">
+						404 NOT
+						<br />
+						<span className="main-accent">FOUND</span>
+					</h1>
+					<p className="heroSubtitleFont">
+						Oops! The page you're looking for
+						<br />
+						doesn't exist.
+					</p>
+				</div>
+			</section>
+			<Footer />
+		</>
+  );
+}
