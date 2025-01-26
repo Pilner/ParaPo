4fd0a6d3319b086c53c7990fc4efb9734f8dbe6b
Railey Macbook Pro <raileyvictuelles@gmail.com>
2025-01-27 02:01:06 +0800

    feat/fix: implement recursive route finding - Recursive Nearest Route finding is using Naive Euclidean Distance - Organized code into separate folders and files

---
+++ src/_data/data.ts
@@ -0,0 +1,2 @@
+export const mapboxAccessToken =
+	process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN ?? "";
---
+++ src/_types/DataPoints.ts
@@ -0,0 +1,6 @@
+import Coordinates from "./Coordinates";
+
+export default interface dataPointsProps {
+	origin: Coordinates;
+	dest: Coordinates;
+}
